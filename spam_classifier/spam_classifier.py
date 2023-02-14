590364e5b91117bc6045ed8aad3d1d14bc0f62c7
Leone Perdigão <leone.lage@gmail.com>
2023-02-14 11:06:03 +0100

    chore: rewrite code for using simple version of neural networks

--- spam_classifier/spam_classifier.py
+++ spam_classifier/spam_classifier.py
@@ -1,24 +1,41 @@
 import numpy as np
-from scipy.optimize import minimize
 
 
 class SpamClassifier:
-    def __init__(self, c=1):
-        self.c = c
-        self.weights = None
-        self.bias = None
+    def __init__(self, hidden_layer_size=13, learning_rate=0.01, epochs=1000):
+        self.hidden_layer_size = hidden_layer_size
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights_input_to_hidden = None
+        self.weights_hidden_to_output = None
 
-    def train(self, x, y):
-        n, d = x.shape
+    @staticmethod
+    def __sigmoid(x):
+        return 1 / (1 + np.exp(-x))
 
-        # minimize the objective function
-        objective = lambda w: 0.5 * np.dot(w, w) + self.c * np.sum(np.maximum(0, 1 - y * (np.dot(x, w) - self.bias)))
-        initial_w = np.zeros(d)
-        self.weights = minimize(objective, initial_w, method='L-BFGS-B', bounds=[(-1, 1) for i in range(d)]).x
+    def train(self, data, features):
+        n_samples, n_features = data.shape
+        self.weights_input_to_hidden = np.random.randn(n_features, self.hidden_layer_size) / np.sqrt(n_features)
+        self.weights_hidden_to_output = np.random.randn(self.hidden_layer_size, 1) / np.sqrt(self.hidden_layer_size)
 
-        # find support vectors and the bias term
-        support_vectors = (1 - y * (np.dot(x, self.weights) - self.bias)) > 0
-        self.bias = np.mean(y[support_vectors] - np.dot(x[support_vectors], self.weights))
+        for _ in range(self.epochs):
+            # forward pass
+            hidden_layer = SpamClassifier.__sigmoid(np.dot(data, self.weights_input_to_hidden))
+            output = SpamClassifier.__sigmoid(np.dot(hidden_layer, self.weights_hidden_to_output))
+
+            # backward pass
+            error = features.reshape(-1, 1) - output
+            d_output = error * output * (1 - output)
+            error_hidden_layer = np.dot(d_output, self.weights_hidden_to_output.T)
+            d_hidden_layer = error_hidden_layer * hidden_layer * (1 - hidden_layer)
+
+            # update weights
+            self.weights_hidden_to_output += self.learning_rate * np.dot(hidden_layer.T, d_output)
+            self.weights_input_to_hidden += self.learning_rate * np.dot(data.T, d_hidden_layer)
 
     def predict(self, data):
-        return np.sign(np.dot(data, self.weights) + self.bias)
+        hidden_layer = SpamClassifier.__sigmoid(np.dot(data, self.weights_input_to_hidden))
+        output = SpamClassifier.__sigmoid(np.dot(hidden_layer, self.weights_hidden_to_output))
+        y_pred = (output > 0.5).astype(int).flatten()
+        return y_pred
+
